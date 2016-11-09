6aea2437035b94e034e17b0a4e6b8b8bacf0d716
Ruthiel Trevisan <ruthiel.trevisan@gmail.com>
2016-11-09 17:42:53 +0000

    Site protected from DoS

--- src/org/academia/SimpleWebServer.java
+++ src/org/academia/SimpleWebServer.java
@@ -3,6 +3,8 @@ package org.academia;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by codecadet on 08/11/16.
@@ -12,19 +14,23 @@ public class SimpleWebServer {
 
     public static void main(String[] args) throws IOException {
 
+
+
         String hostName = "localhost";
         int portNumber = 4000;
 
         ServerSocket serverSocket = new ServerSocket(portNumber);
         System.out.println("À espera de ligação...");
 
+        ExecutorService poll = Executors.newFixedThreadPool(1);
+
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Entrou este IP: " + clientSocket.getRemoteSocketAddress());
-            Thread thread = new Thread(new MultipleClient(clientSocket));
-            thread.start();
+            poll.submit(new MultipleClient(clientSocket));
         }
+
 
     }
 
